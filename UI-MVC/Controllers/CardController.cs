8472bf8e394fe33f1bd92ff70fa578d970f03657
Kobe Ponet <kobe.ponet@student.kdg.be>
2023-12-11 00:29:03 +0100

    added and finished index

---
+++ UI-MVC/Controllers/CardController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MTGM.BL;
+
+namespace UI_MVC.Controllers;
+
+public class CardController : Controller
+{
+    private Manager _manager;
+
+    public CardController(Manager manager)
+    {
+        _manager = manager;
+    }
+    
+    public IActionResult Index()
+    {
+        return View(_manager.GetAllCards());
+    }
+    
+    public IActionResult Add()
+    {
+        return View();
+    }
+    public IActionResult Details(int id)
+    {
+        return View(_manager.GetCard(id));
+    }
+    
+    
+}
--- UI-MVC/Program.cs
+++ UI-MVC/Program.cs
@@ -1,9 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using MTGM.BL;
+using MTGM.DAL;
+using MTGM.DAL.EF;
+using UI_MVC.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Add DbContext, Repository and Manager to the service container
+builder.Services.AddDbContext<MtgmDbContext>(options => options.UseSqlite("Data Source=MTGM.db"));
+builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddScoped<Manager, Manager>();
+
 var app = builder.Build();
+
+// Get the service provider and use it to get the DbContext
+using (var serviceScope = app.Services.CreateScope())
+{
+    var services = serviceScope.ServiceProvider;
+    var context = services.GetRequiredService<MtgmDbContext>();
+
+    // Ensure the database is created
+    context.Database.EnsureCreated();
+
+    // Call the Seed routine if necessary
+    if (!context.Cards.Any())
+    {
+        DataSeeder.Seed(context);
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
