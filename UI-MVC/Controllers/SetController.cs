c1af20b031958060f02a10107a25ea0e264d8744
Kobe Ponet <kobe.ponet@student.kdg.be>
2023-12-20 17:41:42 +0100

    added sets page and API

--- UI-MVC/Controllers/SetController.cs
+++ UI-MVC/Controllers/SetController.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MTGM.BL;
+using System.Linq;
 
 namespace MTGM.UI.MVC.Controllers;
 
 public class SetController : Controller
 {
     private Manager _manager;
-
+    
+    
     public SetController(Manager manager)
     {
         _manager = manager;
@@ -14,5 +16,16 @@ public class SetController : Controller
     public IActionResult Details(int id)
     {
         return View(_manager.GetSet(id));
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var sets = _manager.GetAllSets().ToList();
+        return Ok(sets);
     }
 }
---
+++ UI-MVC/wwwroot/js/sets.js
@@ -0,0 +1,23 @@
+﻿window.onload = function() {
+    fetchSets();
+};
+
+function fetchSets() {
+    fetch('/api/Sets')
+        .then(response => response.json())
+        .then(data => {
+            let table = document.getElementById('setsTable');
+            table.innerHTML = '';
+            data.forEach(set => {
+                let row = table.insertRow();
+                let nameCell = row.insertCell();
+                nameCell.textContent = set.name;
+                let codeCell = row.insertCell();
+                codeCell.textContent = set.code;
+                let releaseDateCell = row.insertCell();
+                releaseDateCell.textContent = set.releaseDate;
+            });
+        });
+}
+
+document.getElementById('refreshButton').addEventListener('click', fetchSets);
